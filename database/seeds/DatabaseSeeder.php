40268e09c70f944875f72aa05765c574ca73262c
oneji <timich1995@gmail.com>
2020-05-22 21:06:14 +0500

    NEW: Seeders.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
-        // $this->call(PizzaCategoriesTableSeeder::class);
-        $this->call(PizzaSizesTableSeeder::class);
+        $this->call([
+            // UserSeeder::class,
+            PizzaCategoriesTableSeeder::class,
+            PizzaSizesTableSeeder::class,
+            PizzasTableSeeder::class
+        ]);
     }
 }
--- database/seeds/PizzasTableSeeder.php
+++ database/seeds/PizzasTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\PizzaSize;
 
 class PizzasTableSeeder extends Seeder
 {
@@ -11,6 +13,17 @@ class PizzasTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Pizza::class, 10)->create();
+        $pizzaSizes = PizzaSize::all();
+        $faker = Faker\Factory::create();
+        
+        factory(App\Pizza::class, 10)->create()->each(function($pizza) use ($pizzaSizes, $faker) {
+            foreach ($pizzaSizes as $size) {
+                $pizza->pizza_sizes()->attach($size->id, [
+                    'price_usd' => $faker->numberBetween($min = 0, $max = 30),
+                    'price_euro' => $faker->numberBetween($min = 0, $max = 30)
+                ]);
+            }
+        });
+
     }
 }
